a5eaacb6e97385a10ae9c1d0f1f2f36d87029adb
Nandak <nandak@nextgen.com>
2019-11-28 14:35:30 +0530

    Updates

---
+++ precache-manifest.9d11d5455489aa942625bca315cc7d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9badf28f156d1c9e90",
+    "url": "/my-online-profile/static/js/runtime~main.0f9badf2.js"
+  },
+  {
+    "revision": "6922bc6badc34ad8ff53",
+    "url": "/my-online-profile/static/js/main.6922bc6b.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-online-profile/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6922bc6badc34ad8ff53",
+    "url": "/my-online-profile/static/css/main.e57a288c.chunk.css"
+  },
+  {
+    "revision": "10536f6e26b60791c65817f12e6f74ea",
+    "url": "/my-online-profile/index.html"
+  }
+];
