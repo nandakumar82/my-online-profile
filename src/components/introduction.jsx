cfaf5cb6292c7fa1dbe4c98097fb004f201e9666
Nandak <nandak@nextgen.com>
2019-11-29 12:58:07 +0530

    my-online-profile : Rename Name

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
                     <p className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Nandu</h1>
+                          <h1>Hi! <br />I'm Nanda</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1mgiNydMj32SoAkUJ15RGLqyeJbEzA5-T/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
